1b8e006d7a3e95abf94c40938ea94be9150fcdf6
carlapardo <carlapardopatino@gmail.com>
2018-07-05 20:41:22 +0200

    Test - firstPositionContainsNumberOne()

--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -1,13 +1,18 @@
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
 	@Test
-	public void renameMe() {
-		assertTrue(true);
+	public void firstPositionContainsNumberOne() {
+		FizzBuzz fizzBuzz = new FizzBuz();
+
+		List<Object> list = fizzBuzz.generateList();
+
+		assertEquals(1, list.get(0));
 	}
 
 }
